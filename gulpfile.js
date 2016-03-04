9c36b32ee30a491a79525ea4ce8052381fa341ba
imfly <kubying@foxmail.com>
2016-03-05 00:41:35 +0800

    Add auto deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,58 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var rename = require("gulp-rename");
+var del = require('del');
 
 // Settings
 var settings = {
-    destFolder: '.'
-}
+  destFolder: '.',
+  sources: 'sources/electron',
+  tmp: 'sources/tmp'
+};
 
 // Path map
 var languages = {
-    en: 'sources/electron/docs/**/*.*',
-    translations:  'sources/electron/docs-translations/**/*.*'
+  en: 'sources/electron/docs/**/*.*',
+  translations: 'sources/electron/docs-translations/**/*.*'
 };
 
 // Copy en
 gulp.task('copy-en', function() {
-    return gulp.src(languages.en)
-        .pipe(gulp.dest(settings.destFolder + '/en/'));
+  return gulp.src(languages.en)
+    .pipe(gulp.dest(settings.tmp + '/en/'));
 });
 
 // Copy translations
-gulp.task('copy-tr', function() {
-    return gulp.src(languages.translations)
-        .pipe(gulp.dest(settings.destFolder));
+gulp.task('copy-tr', ['copy-en'], function() {
+  return gulp.src(languages.translations)
+    .pipe(gulp.dest(settings.tmp));
+});
+
+// Rename
+gulp.task('rename', ['copy-tr'], function() {
+  // rename via function
+  gulp.src(settings.tmp + "/**/README.md")
+    .pipe(rename({
+      basename: "SUMMARY",
+      extname: ".md"
+    }))
+    .pipe(gulp.dest(settings.tmp));
+});
+
+// Copy tmp
+gulp.task('copy-tmp', ['rename'], function() {
+  return gulp.src(settings.tmp + '/**')
+    .pipe(gulp.dest(settings.destFolder));
 });
 
 // Copy
-gulp.task('copy', ['copy-en', 'copy-tr']);
-
-// Rename todo: gulp-rename
-gulp.task('rename', function() {
-
+gulp.task('copy', ['copy-tmp'], function() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del([settings.tmp + '/**'], {force: true});
 });
 
 //Deploy
 gulp.task('deploy', function() {
-    return gulp.src('./_book/**/*')
-        .pipe(ghPages());
+  return gulp.src('./_book/**/*')
+    .pipe(ghPages());
 });
